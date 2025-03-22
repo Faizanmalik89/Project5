c405b51594227e6c7536a54249bf87d1d1ff7719
MALIK <134768946+Faizanmalik89@users.noreply.github.com>
2025-03-22 07:54:58 +0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,14 @@
-// Import Firebase modules
+// Firebase SDK imports
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-// Your Firebase configuration
+// Your Firebase Configuration
 const firebaseConfig = {
     apiKey: "AIzaSyAeopZBOH519qVwLkAL06RttMG-wyfAC5M",
     authDomain: "realistic-c5616.firebaseapp.com",
     projectId: "realistic-c5616",
-    storageBucket: "realistic-c5616.firebasestorage.app",
+    storageBucket: "realistic-c5616.appspot.com",
     messagingSenderId: "809481702160",
     appId: "1:809481702160:web:10a82f267efb5dcc137395",
     measurementId: "G-HMY8J90DEQ"
@@ -19,5 +19,5 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-// Export auth and db for use in other files
+// Export Firebase auth and database
 export { auth, db };
